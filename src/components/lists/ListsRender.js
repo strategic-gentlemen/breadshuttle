29d9a9cca2f2637827ab611f16d0011211600a7a
Jake <dmust32@gmail.com>
2019-07-23 22:12:56 -0600

    Built out the beginnings of the listRender component.

--- src/components/lists/ListsRender.js
+++ src/components/lists/ListsRender.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import ListCard from './ListCard';
+
+const dummyLists = [
+  {
+    id: 1,
+    listName: 'test1',
+  },
+  {
+    id: 2,
+    listName: 'test2'
+  },
+  {
+    id: 3,
+    listName: 'test3'
+  }
+];
 
 const ListRender = (props) => {
+  const mapListItems = () => {
+    return dummyLists.map(list => {
+      return (
+        <ListCard 
+          listName={list.listName}
+        />
+      )
+    })
+  }
+
   return (
-    <div className="testing">
-      List Render
+    <div className="lists-container">
+      <h1>My Lists</h1>
+      {mapListItems()}
     </div>
   );
 }
