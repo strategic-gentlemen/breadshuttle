a6b03c1dfc8032f3f7bb57c13770e39910fe612a
Justus Ray <justusmray@gmail.com>
2019-09-29 09:55:21 -0600

    finished all of the login views besides the splash screen

--- src/components/register/Register.js
+++ src/components/register/Register.js
@@ -1,40 +1,66 @@
 import React from 'react';
+import NavBar from '../common/NavBar';
+import Button from '../common/Button';
 
 const Register = (props) => {
+  
   return (
+      
     <div className='auth-container'>
-      <div className='auth-top'>
-        <div className='place-holder logo'></div>
+      <NavBar/>
+
+      <div className='header-text'>
+        Sign Up
 
       </div>
-      <div className='auth-bottom'>
-
-        <div className='auth-input'>
-          <div className='icon-box'>
-
-          </div>
-          <input type='text' placeholder='Email'/>
-
-        </div>
-        <div className='auth-input'>
-          <div className='icon-box'>
-            
-          </div>
-          <input type='password' placeholder='Password'/>
-
-        </div>
-        <button onClick={() => {
-          props.history.push('/')
-        }}>Register</button>
-        <br/>
+      
         
-        <div>
-          or
-          <br/>
-          <a href='/login'>Login</a>
-        </div>
-      </div>
+      <div className='auth-form-container'>
+        <form 
+          
+          className='auth-form'>
+          <div>
 
+            <div className='auth-input'>
+              <label for='fname'>First Name</label>
+              <input id='fname' type='text'/>
+            </div>
+
+            <br/>
+
+            <div className='auth-input'>
+              <label for='lname'>Last Name</label>
+              <input id='lname' type='text'/>
+              
+            </div>
+            <br/>
+
+            <div className='auth-input'>
+              <label for='email'>Email</label>
+              <input id='email' type='text'/>
+              
+            </div>
+            <br/>
+
+            <div className='auth-input'>
+              <label for='password'>Password</label>
+              <input id='password' type='password'/>
+              
+            </div>
+          </div>
+          <div>
+            <Button
+              title='Next' 
+              onClick={() => {props.history.push('/register2')}}
+              type='button'>
+              
+                
+            </Button>
+          </div>
+            
+          
+        </form>
+      </div>
     </div>
   );
 }
